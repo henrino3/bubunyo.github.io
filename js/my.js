207a1f1c1ed40e10dd180fe948c8d0c287c7c9c1
Sam Nyavor <samnyavor@gmail.com>
2015-09-05 19:11:51 +0000

    Added typed.js, removed about and contact pages

--- js/my.js
+++ js/my.js
@@ -1,15 +1,7 @@
 
-function sticky_relocate() {
-  var window_top = $(window).scrollTop();
-  var div_top = $('#sticky-anchor').offset();
-  if (window_top > div_top) {
-    $('#sticky').addClass('stick');
-  } else {
-    $('#sticky').removeClass('stick');
-  }
-}
-
-$(function() {
-  $(window).scroll(sticky_relocate);
-  sticky_relocate();
-});
+    $(function(){
+        $(".element").typed({
+            strings: ["First sentence.", "Second sentence."],
+            typeSpeed: 0
+        });
+    });
